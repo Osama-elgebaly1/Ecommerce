7cc65abe7f8ee79f05c5f35de7ec6f9a2b240023
osama <osamaelgebaly122@gmail.com>
2024-11-06 01:02:12 +0200

    ecommerce app is added

--- project/settings.py
+++ project/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ecommerce.apps.EcommerceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
