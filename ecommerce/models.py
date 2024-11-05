d0f7c3db6c624739ee587b634e6eb1a0b29c899a
osama <osamaelgebaly122@gmail.com>
2024-11-06 01:37:47 +0200

    The models are added ( Product , Customer , Order ,Category )

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,46 @@
 from django.db import models
-
+import datetime 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    def __str__(self) -> str:
+        return self.name
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6,decimal_places=2)
+    description = models.TextField(max_length=250,default='',blank=True,null=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/')
+    def __str__(self) -> str:
+        return self.name
+
+
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.first_name
+    
+
+class Order(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    quantity = models.IntegerField(default = 1)
+    address = models.CharField(max_length=200,)
+    phone = models.CharField(max_length=20)
+    date = models.DateField(auto_now_add=True)
+    status = models.BooleanField(default=False)
+
+    def __str__(self) :
+        return self.product
