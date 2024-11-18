9270d9e80212f68136b61596d14c6d0e7a41b743
osama <osamaelgebaly122@gmail.com>
2024-11-18 04:21:13 +0200

    Add products to cart and session

--- cart/cart.py
+++ cart/cart.py
@@ -5,11 +5,25 @@ class Cart():
         self.session = request.session
 
         # if new user , there is no  session key , let's create one
-        if 'session_key' not in request.session:
+        if 'session_key' not in self.session:
             cart = self.session['session_key'] = {}
 
         cart = self.session.get('session_key')
 
-        # make sure cart is available on all pages of the site 
+        self.cart = cart
+
+    def add(self,product):
+        product_id = str(product.id)
+
+        if product.id in self.cart:
+            pass
+
+        else:
+            self.cart[product_id] = {'price':str(product.price)}
+
+        self.session.modified = True
+
+
+        
 
 
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from ecommerce.models import Category
+from .cart import Cart
+from ecommerce.models import Product
+from django.http import JsonResponse
 # Create your views here.
 
 def summary(request):
@@ -9,7 +12,21 @@ def summary(request):
 
 
 def add(request):
-    pass
+    # Get the cart
+    cart = Cart(request)
+    #  test for POST
+    if request.POST.get('action') == 'post':
+        # get stuff
+        product_id = int(request.POST.get('product_id'))
+        # lookup product in DB
+        product = get_object_or_404(Product, id=product_id)
+        # save to session
+        cart.add(product=product)
+        # return response 
+        response = JsonResponse({'product name :': product.name})
+        return response
+
+    
 
 
 
