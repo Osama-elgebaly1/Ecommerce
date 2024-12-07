9126d65f5ce7f4c6dd977c286de1561a17bab5aa
osama <osamaelgebaly122@gmail.com>
2024-12-08 01:08:40 +0200

    Adding Order models

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from  ecommerce.models import Product
+
+
+
 
 class ShippingAddress(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
@@ -16,6 +21,39 @@ class ShippingAddress(models.Model):
 
         def __str__(self) :
             return f"Shipping address  {str(self.id)}"
+        
+        
+def create_shipping(sender,instance,created,**kwargs):
+    if created:
+        shipping_address = ShippingAddress(user=instance)
+        shipping_address.save()
 
+post_save.connect(create_shipping,sender=User)
+
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    full_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=250)
+    shipping_address = models.TextField(max_length=20000)
+    amount_paid = models.DecimalField(decimal_places=2,max_digits=10)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"order {str(self.id)}"
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE,null=True,blank=True)
+    product = models.ForeignKey(Product,null=True,blank=True,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    quantity = models.PositiveBigIntegerField(default=
+                                              1)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+
+    def __str__(self):
+        return f"order Item {str(self.id)}"
+    
 
 
--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-
+from cart.cart import Cart
+from payment.forms import ShippingForm,ShippingAddress
 # Create your views here.
 
 def payment_success(request):
     return render(request,'payment/payment_success.html',{})
+
+def checkout(request):
+    cart = Cart(request)
+    products = cart.get_prods
+    quantities = cart.get_quants
+    total = cart.total()
+    if request.user.is_authenticated:
+        current_user  = ShippingAddress.objects.get(user_id = request.user.id)
+        shipping_form = ShippingForm(request.POST or None ,instance=current_user)
+    else:
+        shipping_form = ShippingForm(request.POST or None)
+
+        
+
+    return render(request,'payment/checkout.html',{
+                                       'products':products,
+                                       'quantities':quantities,
+                                       'total':total,
+                                       'shipping_form':shipping_form})
+
